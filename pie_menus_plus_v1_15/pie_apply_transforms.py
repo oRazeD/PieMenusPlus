9bfc39f9a70f14f557c5306ef51f37930b714c3c
Ethan Simon-Law <ethan.simon.3d@gmail.com>
2020-07-24 19:38:16 -0400

    Optimization, moved GLP licensing

--- pie_menus_plus_v1_15/pie_apply_transforms.py
+++ pie_menus_plus_v1_15/pie_apply_transforms.py
@@ -1,3 +1,54 @@
+import bpy
+from bpy.props import EnumProperty
+
+
+class PIESPLUS_OT_transforms(bpy.types.Operator):
+    bl_idname = "pies_plus.transforms"
+    bl_label = "Apply / Clear Transforms"
+    bl_description = "Applys / Clears transforms based on the selected method"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    tranforms_type: EnumProperty(items=(('apply_loc', "Apply Location", ""),
+                                          ('apply_rot', "Apply Rotation", ""),
+                                          ('apply_scale', "Apply Scale", ""),
+                                          ('apply_rot_scale', "Apply Rot & Scale", ""),
+                                          ('apply_all', "Apply All", ""),
+                                          ('clear_all', "Clear All", "")), name = 'Transform Type')
+
+    def execute(self, context):
+        if self.tranforms_type == 'apply_loc':
+            bpy.ops.object.transform_apply(rotation=False, scale=False)
+
+        elif self.tranforms_type == 'apply_rot':
+            bpy.ops.object.transform_apply(location=False, scale=False)
+
+        elif self.tranforms_type == 'apply_scale':
+            bpy.ops.object.transform_apply(location=False, rotation=False)
+
+        elif self.tranforms_type == 'apply_rot_scale':
+            bpy.ops.object.transform_apply(location=False)
+
+        elif self.tranforms_type == 'apply_all':
+            bpy.ops.object.transform_apply()
+
+        else:
+            for ob in context.selected_objects:
+                ob.rotation_euler, ob.location, ob.scale = (0,0,0), (0,0,0), (1,1,1)
+        return {'FINISHED'}
+
+
+##############################
+#   REGISTRATION    
+##############################
+
+
+def register():
+    bpy.utils.register_class(PIESPLUS_OT_transforms)
+
+def unregister():
+    bpy.utils.unregister_class(PIESPLUS_OT_transforms)
+
+
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  This program is free software; you can redistribute it and/or
@@ -15,54 +66,3 @@
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 # ##### END GPL LICENSE BLOCK #####
-
-
-import bpy
-from bpy.props import EnumProperty
-
-
-class PIESPLUS_OT_apply_clear_transforms(bpy.types.Operator):
-    bl_idname = "pies_plus.apply_transforms"
-    bl_label = "Apply Transforms"
-    bl_description = "Applys / Clears transforms with the corresponding selected method"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    apply_transforms: EnumProperty(items=(('apply_loc', "Apply Location", ""),
-                                          ('apply_rot', "Apply Rotation", ""),
-                                          ('apply_scale', "Apply Scale", ""),
-                                          ('apply_rot_scale', "Apply Rot & Scale", ""),
-                                          ('apply_all', "Apply All", ""),
-                                          ('clear_all', "Clear All", "")))
-
-    def execute(self, context):
-        if self.apply_transforms == 'apply_loc':
-            bpy.ops.object.transform_apply(rotation=False, scale=False)
-
-        elif self.apply_transforms == 'apply_rot':
-            bpy.ops.object.transform_apply(location=False, scale=False)
-
-        elif self.apply_transforms == 'apply_scale':
-            bpy.ops.object.transform_apply(location=False, rotation=False)
-
-        elif self.apply_transforms == 'apply_rot_scale':
-            bpy.ops.object.transform_apply(location=False)
-
-        elif self.apply_transforms == 'apply_all':
-            bpy.ops.object.transform_apply()
-
-        else:
-            for ob in context.selected_objects:
-                ob.rotation_euler, ob.location, ob.scale = (0,0,0), (0,0,0), (1,1,1)
-        return {'FINISHED'}
-
-
-##############################
-#   REGISTRATION    
-##############################
-
-
-def register():
-    bpy.utils.register_class(PIESPLUS_OT_apply_clear_transforms)
-
-def unregister():
-    bpy.utils.unregister_class(PIESPLUS_OT_apply_clear_transforms)
