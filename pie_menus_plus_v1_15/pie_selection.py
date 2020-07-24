333a4a8886470d6e174d8a316967773440297c81
Ethan Simon-Law <ethan.simon.3d@gmail.com>
2020-07-24 19:49:38 -0400

    Redesigned 'Select Marked' for better usability

--- pie_menus_plus_v1_15/pie_selection.py
+++ pie_menus_plus_v1_15/pie_selection.py
@@ -1,22 +1,3 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
-
-
 import bpy
 from bpy.types import Operator
 from bpy.props import EnumProperty
@@ -72,8 +53,21 @@ class PIESPLUS_OT_mesh_selection(Operator):
         else:
             if context.active_object:
                 if context.object.mode == 'EDIT':
+                    if context.preferences.addons[__package__].preferences.invertSelection_Pref:
+                        scene_verts = context.scene.statistics(context.view_layer).split(" | ")[1]
+                        if scene_verts.split('/')[1] == scene_verts.split('/')[0].split(':')[1]:
+                            bpy.ops.mesh.select_all(action='DESELECT')
+                        else:
+                            bpy.ops.mesh.select_all(action='SELECT')
+                        return {'FINISHED'}
                     bpy.ops.mesh.select_all(action='TOGGLE')
                     return {'FINISHED'}
+            if context.preferences.addons[__package__].preferences.invertSelection_Pref:
+                if len([ob for ob in context.view_layer.objects if ob.visible_get()]) == len(context.selected_objects):
+                    bpy.ops.object.select_all(action='DESELECT')
+                else:
+                    bpy.ops.object.select_all(action='SELECT')
+                return {'FINISHED'}
             bpy.ops.object.select_all(action='TOGGLE')
         return {'FINISHED'}
 
@@ -93,27 +87,37 @@ class PIESPLUS_OT_select_loop_inner_region(Operator):
         return {'FINISHED'}
 
 
-class PIESPLUS_OT_select_marked(Operator):
-    bl_idname = 'pies_plus.select_marked_sharp'
-    bl_label = ""
+class PIESPLUS_OT_select_seamed(Operator):
+    bl_idname = 'pies_plus.select_seamed'
+    bl_label = "Select all edges with seams"
     bl_options = {'REGISTER', 'UNDO'}
-
-    markedChoice: EnumProperty(items=(('marked_sharp', "Sharps", ""),
-                                      ('marked_seam', "Seams", "")),
-                               default='marked_sharp')
 
     def execute(self, context):
         bpy.ops.object.mode_set(mode='EDIT')
         bpy.ops.mesh.select_mode(type='EDGE')
         bpy.ops.object.mode_set(mode='OBJECT')
-        mesh = context.object.data
 
-        for edge in mesh.edges:
-            if self.markedChoice == 'marked_sharp':
-                if edge.use_edge_sharp:
-                    edge.select = True
-            else:  # Seam
+        for ob in context.selected_objects:
+            for edge in ob.data.edges:
                 if edge.use_seam:
+                    edge.select = True
+
+        bpy.ops.object.mode_set(mode='EDIT')
+        return {'FINISHED'}
+
+class PIESPLUS_OT_select_sharped(Operator):
+    bl_idname = 'pies_plus.select_sharped'
+    bl_label = "Select all edges with sharps"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_mode(type='EDGE')
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+        for ob in context.selected_objects:
+            for edge in ob.data.edges:
+                if edge.use_edge_sharp:
                     edge.select = True
 
         bpy.ops.object.mode_set(mode='EDIT')
@@ -139,7 +143,8 @@ class PIESPLUS_OT_make_links(Operator):
 classes = (PIESPLUS_OT_view_selection,
            PIESPLUS_OT_mesh_selection,
            PIESPLUS_OT_select_loop_inner_region,
-           PIESPLUS_OT_select_marked,
+           PIESPLUS_OT_select_seamed,
+           PIESPLUS_OT_select_sharped,
            PIESPLUS_OT_make_links)
 
 
@@ -151,3 +156,22 @@ def register():
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
+
+
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
