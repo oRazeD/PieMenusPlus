a75b94a283e77d53aa5c46b9d1981b06d876de8f
Ethan Simon-Law <ethan.simon.3d@gmail.com>
2020-11-05 05:55:06 -0500

    Version Number Change, Align Pie Setup

--- pie_menus_plus_v1_2/__init__.py
+++ pie_menus_plus_v1_2/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {"name": "Pie Menus Plus",
            "description": "Additional / Improved Pie Menus for Blender 2.8+",
            "author": "Ethan Simon-Law",
-           "version": (1, 2, 0),
+           "version": (1, 25),
            "blender": (2, 90, 0),
            "tracker_url": "https://discord.com/invite/wHAyVZG",
            "category": "3D View"}
@@ -22,7 +22,8 @@ module_names = ("ui",
                 "pie_shading",
                 "pie_proportional",
                 "pie_keyframing",
-                "pie_save")
+                "pie_save",
+                "pie_align")
 
 modules = []
 
